a01950618cefa0bc209722a67a68f66444412093
jyobb <jyobb1@gmail.com>
2016-04-13 15:22:21 -0600

    added result delete, fixed drush

--- theme/islandora-metadata-analyzer-result-list.tpl.php
+++ theme/islandora-metadata-analyzer-result-list.tpl.php
@@ -6,8 +6,7 @@
 
 
 <?php foreach( $variables['resultArray'] as $id => $name ){ ?>
-    <p><a href="elementCount/<?php echo $id;?>"><?php echo $name; ?></a></p>
-
+    <p><a href="elementCount/<?php echo $id;?>"><?php echo $name; ?></a><a href="deleteResult/<?php echo $id;?>"><?php echo " | Delete: " . $name; ?></a></p>
 <?php } 
 
 
